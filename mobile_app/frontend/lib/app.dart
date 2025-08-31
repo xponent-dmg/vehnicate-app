db7f9f8977982dd6364f4d7ea766025cfde9b48e
xponent-dmg <harishankar.r2102@gmail.com>
2025-08-31 19:42:53 +0530

    feat: static profile page

--- mobile_app/frontend/lib/app.dart
+++ mobile_app/frontend/lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vehnicate_frontend/Pages/homepage.dart';
+import 'package:vehnicate_frontend/Pages/profile_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -29,7 +30,7 @@ class App extends StatelessWidget {
           ),
         ),
       ),
-      home: const Homepage(),
+      home: ProfilePage(),
     );
   }
 }
