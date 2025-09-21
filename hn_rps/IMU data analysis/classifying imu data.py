3ef7f17a504c0c171a06cbbe45a3e09bbe76da7d
Sai Siddharth Sottallu <ss.sottallu@gmail.com>
2025-09-21 21:10:57 +0530

    updated FFT & low pass filter to identify lane chanes

--- hn_rps/IMU data analysis/classifying imu data.py
+++ hn_rps/IMU data analysis/classifying imu data.py
@@ -6,6 +6,7 @@ import numpy as np
 import math
 from scipy.stats import skew,kurtosis
 import csv
+from scipy.signal import butter, sosfiltfilt
 
 # Load CSV
 df = pd.read_csv("D:\\Vehnicate\\Prototype\\data\\testdrive5\\sensor.csv", header=None)
@@ -104,7 +105,7 @@ while not end:
     initial_pointer=pointer
     s=df['Yaw'][initial_pointer]
 
-    if abs(df['Yaw'][pointer]-df['Yaw'][pointer+def_ChunkSize-1])<= 0.1: #0.5 degree
+    if abs(df['gyro_z'][pointer]-df['gyro_z'][pointer+def_ChunkSize-1])<= 0.1: #0.5 degree
         pointer+=def_ChunkSize
     else:
         pointer+=def_ChunkSize
@@ -118,13 +119,56 @@ while not end:
         
     
     if def_ChunkSize>l-pointer-1:
-        def_ChunkSize=l-pointer-1
+        def_ChunkSize=l-pointer
         end=True
         pointer+=def_ChunkSize #points to the start of the beginning of the next chunk.
         
     Chunks[initial_pointer] = pointer-1
+#print(Chunks)
 
 
+L=len(df['time_ms'])
+Chunks_lanes={}
+# **merging 2 arc chunks to together form potential lane change**
+i1=0
+e1=Chunks[i1]
+i2 = e1+1
+e2 = Chunks[i2]
+d_yaw1 = abs(df['Yaw'][e1]-df['Yaw'][i1])
+d_yaw2 = abs(df['Yaw'][e2]-df['Yaw'][i2])
+while True:
+    initial_yaw=df['Yaw'][i1]
+    while abs(e1-i1>9) and abs(e2-i2>9) and (d_yaw1 < 30) and (d_yaw1 > 1.5) and (d_yaw2 < 30) and (d_yaw2 > 1.5):# and abs(df['Yaw'][e2] - df['Yaw'][i1]) < 2:
+        print(df['time_ms'][e1])
+        Chunks_lanes[i1] = e2
+        del Chunks[i1]
+        i1 = i2
+        e1=Chunks[i1]
+        i2 = e1+1
+        try:
+            e2 = Chunks[i2]
+            if df['Yaw'][e2]>initial_yaw:
+                print(df['time_ms'][e2])
+                #print(df['time_ms'][i1])
+                #print(df['time_ms'][i2])
+                for i in range(i2,e2):
+                    if df['yaw'][i]==i1:
+                        e2 = i
+        except:
+            break
+        d_yaw1 = abs(df['Yaw'][e1]-df['Yaw'][i1])
+        d_yaw2 = abs(df['Yaw'][e2]-df['Yaw'][i2])
+    #else:
+    #    i1=i2
+    i1 = i2
+    e1=Chunks[i1]
+    i2 = e1+1
+    try:
+        e2 = Chunks[i2]
+    except:
+        break
+    d_yaw1 = abs(df['Yaw'][e1]-df['Yaw'][i1])
+    d_yaw2 = abs(df['Yaw'][e2]-df['Yaw'][i2])
 
 ################## "Chunks" is where the chunks are stored. Format- dictionary with:
 ################## key = starting position of each chunk (index starts from 0),
@@ -137,7 +181,22 @@ while not end:
 # new format- Chunks={[start, end]:[end-start+1(length of chunk), [mean, sd, variance, skewness, time autocorrelation, etc](of gyro_z),[mean,sd,var,etc](of yaw)],...}
 
 Chunks01={}
+road_curvature=[]
+initial = 0
 for i in Chunks:
+    #approximating the road curvature wrt time
+    initYaw = df['Yaw'][initial]
+    #if df['Yaw'][Chunks[i]]-initYaw > 
+    l=[]
+    l.append(i)
+    l.append(Chunks[i])
+    if Chunks[i]-i>9:
+        l.append(min(df['Yaw'][i],df['Yaw'][Chunks[i]]))
+    else:
+        l.append(np.mean(df['Yaw'][i:Chunks[i]+1]))
+    road_curvature.append(l)
+
+    #computing the statistics of gyro_z for each chunk
     Chunks01[(
         (i,Chunks[i]),
         (df['time_ms'][i],df['time_ms'][Chunks[i]])
@@ -183,7 +242,6 @@ for i in Chunks:
 #    print(i, '\t',Chunks01[i])
 #print(len(Chunks01))
 
-
 ### plotting data
 #plt.subplot(2,1,1)
 plt.plot(df['time_ms'],Yaw, label='yaw')
@@ -196,9 +254,15 @@ plt.legend()
 for i in Chunks:
     plt.plot(df['time_ms'][i],df['Yaw'][i],marker='o',color='green')
     plt.plot(df['time_ms'][Chunks[i]],df['Yaw'][Chunks[i]],marker='o',color='red')
+for i in Chunks_lanes:
+    plt.plot(df['time_ms'][i],df['Yaw'][i],marker='o',color='blue')
+    plt.plot(df['time_ms'][Chunks_lanes[i]],df['Yaw'][Chunks_lanes[i]],marker='o',color='yellow')
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+
+
+
 
 #format of the content stores in chunk01:
 #((30607, 30616), (733402, 733637))       [10, [-0.012, 0.009797958971132713, 9.6e-05, 1.2405649780820631, -0.010416666666667407]]
@@ -222,11 +286,80 @@ for i in Chunks01:
     l.append(Chunks01[i][3][3])
     l.append(Chunks01[i][3][4])
     data.append(l)
-
 with open(r"D:\Vehnicate\Prototype\Code\python\IMU data analysis\data resulting from code\statistics of testdrive.csv", "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerows(data)
 
+
+#############statistics of potential lane changes
+Chunks01={}
+for i in Chunks_lanes:
+    Chunks01[(
+        (i,Chunks_lanes[i]),
+        (df['time_ms'][i],df['time_ms'][Chunks_lanes[i]])
+        )] = [] #this is the placeholder for mean,std,var,skew,kurtosis
+    
+    #appending the number of ROWS in each chunk
+    Chunks01[
+        (i,Chunks_lanes[i]),
+        (df['time_ms'][i],df['time_ms'][Chunks_lanes[i]])
+        ].append(Chunks_lanes[i]-i+1)
+    
+    #appending the lifetime of that chunk in milliseconds
+    Chunks01[
+        (i,Chunks_lanes[i]),
+        (df['time_ms'][i],df['time_ms'][Chunks_lanes[i]])
+        ].append(df['time_ms'][Chunks_lanes[i]] - df['time_ms'][i])
+    
+    #appending the change in yaw during that chunk
+    Chunks01[
+        (i,Chunks_lanes[i]),
+        (df['time_ms'][i],df['time_ms'][Chunks_lanes[i]])
+        ].append(df['Yaw'][Chunks_lanes[i]] - df['Yaw'][i])
+    #add mean
+    l_gz=[]
+    l_gz.append(np.mean(df['gyro_z'][i:Chunks_lanes[i]+1]))
+
+    #add population standard deviation
+    l_gz.append(np.std(df['gyro_z'][i:Chunks_lanes[i]+1],ddof=0))
+
+    #add variance
+    l_gz.append(np.var(df['gyro_z'][i:Chunks_lanes[i]+1],ddof=0))
+
+    #add skewness
+    l_gz.append(skew(df['gyro_z'][i:Chunks_lanes[i]+1],bias=False))
+
+    #add kurtosis
+    l_gz.append(kurtosis(df['gyro_z'][i:Chunks_lanes[i]+1],fisher=True))
+
+    Chunks01[(i,Chunks_lanes[i]),
+        (df['time_ms'][i],df['time_ms'][Chunks_lanes[i]])].append(l_gz)
+
+
+data = []
+columns = ['ChunkStart_index','ChunkEnd_index','ChunkStart_time','ChunkEnd_time','ChunkSize','ChunkLifetime','d(yaw)','mean_gz','std_gz','var_gz','skewness_gz','kurtosis_gz']
+data.append(columns)
+for i in Chunks01:
+    l = []
+    l.append(i[0][0])
+    l.append(i[0][1])
+    l.append(i[1][0])
+    l.append(i[1][1])
+    l.append(Chunks01[i][0])
+    l.append(Chunks01[i][1])
+    l.append(Chunks01[i][2])
+    l.append(Chunks01[i][3][0])
+    l.append(Chunks01[i][3][1])
+    l.append(Chunks01[i][3][2])
+    l.append(Chunks01[i][3][3])
+    l.append(Chunks01[i][3][4])
+    data.append(l)
+with open(r"D:\Vehnicate\Prototype\Code\python\IMU data analysis\data resulting from code\statistics of potential lane changes.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
+
+
+########statistics of non-straight chunks (also includes potential lane changes ofc)
 df1 = pd.read_csv(r"D:\Vehnicate\Prototype\Code\python\IMU data analysis\data resulting from code\statistics of testdrive.csv", header=None)
 df1.columns = columns
 l1 = len(df1)
@@ -244,3 +377,8 @@ for i in range(1, l1):
 with open(r"D:\Vehnicate\Prototype\Code\python\IMU data analysis\data resulting from code\statistics of non-st chunks.csv", "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerows(data)
+
+
+
+
+
--- hn_rps/IMU data analysis/low pass filter for yaw.py
+++ hn_rps/IMU data analysis/low pass filter for yaw.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import butter, filtfilt
+import numpy as np
+from scipy.fft import fft, fftfreq
 
 # Load and slice
 df = pd.read_csv(r"D:\Vehnicate\Prototype\Code\python\IMU data analysis\data resulting from code\csv file with yaw.csv")
 df.columns = ['time_ms', 'acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z','Yaw']
 
-df = df.iloc[19701:25000].copy()  # segment
+#df = df.iloc[19701:25000].copy()  # segment
+#df = df.iloc[7722:10820].copy()
+#df = df.iloc[6507:7177].copy()
+df = df.iloc[13264:16200].copy()
+
 
 # Convert time
 df["time_ms"] = pd.to_numeric(df["time_ms"], errors="coerce")
@@ -25,8 +31,88 @@ def butter_lowpass_filter(data, cutoff, fs, order=4):
     b, a = butter(order, normal_cutoff, btype='low')
     return filtfilt(b, a, data)
 
-cutoff = 0.184  # Hz (adjust this for yaw smoothing)
+
+# --- FFT with detrending + window ---
+y = df_resampled["Yaw"].values
+N = len(y)
+T = 1.0 / fs
+
+# Detrend (remove mean)
+y_detrended = y - np.mean(y)
+
+# Apply Hann window
+window = np.hanning(N)
+y_win = y_detrended * window
+
+# FFT
+yf = fft(y_win)
+xf = fftfreq(N, T)[:N//2]
+print(type(xf),"type_xf")
+# Correct magnitude spectrum (single-sided, with window compensation)
+magnitude = 2.0 / N * np.abs(yf[:N//2])
+magnitude /= window.mean()   # compensate for Hann window loss
+
+#print(np.mean(xf),'mean')
+#sum_mag,count = 0,0
+#sum_xf,count1=0,0
+xf1=[]
+for i in range(len(xf)):
+    try:
+        xf1.append(xf[i]-xf[i+1])
+    #    sum_xf += (magnitude[i+1]-magnitude[i])/(xf[i+1]-xf[i])
+    #    count1+=1
+    except:
+        xf1.append(0)
+        break
+    #if (xf[i]<=np.mean(xf)):
+    #    sum_mag+=(magnitude[i])
+    #    count+=1
+#print(sum_mag/count,"mean of mag = threshold")
+#print(sum_xf/count1,"threshold for xf")
+##print(xf1[:10],"xf1")
+#print(xf[:10],"xf")
+
+xf1 = np.array(xf1)
+
+mask = magnitude > 0.02
+xf = xf[mask]
+magnitude = magnitude[mask]
+xf1 = xf1[mask]
+
+a=(np.mean(xf*magnitude)/np.mean(magnitude))
+#print((np.sqrt(np.mean(xf**2))+a)/2)
+#print(np.mean(xf/np.sqrt(magnitude))*np.mean(magnitude),"this")
+
+# --- Plot ---
+#plt.figure(figsize=(10,5))
+#plt.subplot(1,2,1)
+#plt.plot(xf, magnitude, color="blue")
+#plt.xlim(0, fs/2)   # up to Nyquist frequency
+#plt.xlabel("Frequency [Hz]")
+#plt.ylabel("Magnitude")
+#plt.title("FFT of Yaw (detrended + Hann window)")
+#plt.grid(True)
+#plt.show()
+
+#plt.subplot(1,2,2)
+#plt.plot(xf,(np.exp(-1*xf*xf1))/(magnitude**magnitude))
+#plt.xlabel("frequency")
+#plt.ylabel("xf/magnitude")
+#plt.show()
+
+#print(((np.exp(-1*xf*xf1))/(magnitude**magnitude))[0],"testing")
+#print(max((np.exp(-1*xf*xf1))/(magnitude**magnitude)),"max")
+
+function = (np.exp(-1*xf*xf1))/(magnitude**magnitude)
+for i in range(len(function)):
+    if function[i]>=max(function):
+        cutoff = (xf[i])
+        break
+
+
+#cutoff = 0.404 # Hz (adjust this for yaw smoothing)
 df_resampled["filtered"] = butter_lowpass_filter(df_resampled["Yaw"].values, cutoff, fs)
+#print(np.mean(df_resampled["Yaw"]))
 
 # Plot
 plt.figure(figsize=(10,5))
