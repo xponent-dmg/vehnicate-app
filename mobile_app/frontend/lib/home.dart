70fe60d0c809695d09b448f10f41518c36e1e705
Embontooth <dhruvsamuel05@gmail.com>
2026-01-12 23:42:36 +0530

    Changed the sensor debug page to map page

--- mobile_app/frontend/lib/home.dart
+++ mobile_app/frontend/lib/home.dart
@@ -55,7 +55,7 @@ class _HomeState extends State<Home> {
           }
         },
         // children: [DashboardPage(), MapPage(key: _mapPageKey), GaragePage(), DriveAnalyzePage()],
-        children: [DashboardPage(), SensorDebugPage(), GaragePage(), DriveAnalyzePage()],
+        children: [DashboardPage(), MapPage(), GaragePage(), DriveAnalyzePage()],
       ),
       bottomNavigationBar: GnavBar(selectedIndex: selectedIndex, onTabChange: onTabChange),
     );
