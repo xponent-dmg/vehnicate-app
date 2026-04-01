d7d8634258aec49d322f294d21b4ee1a04288598
xponent-dmg <harishankar.r2102@gmail.com>
2026-04-02 02:28:38 +0530

    redesigned the verification email page

--- mobile_app/frontend/android/app/build.gradle.kts
+++ mobile_app/frontend/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "com.vehnicate.app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "26.3.11579264"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
